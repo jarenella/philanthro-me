f386a251ecc8ce81565f9931a970d7189d52d478
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2023-01-11 21:59:54 -0700

    created first mutation for ADD_USER

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;

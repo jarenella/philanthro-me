45a07b0916bce903b7050fa60d1ce2a19ab1e610
ReTims <renatatims@gmail.com>
2023-01-12 10:04:38 -0600

    Added basic search form using tailwind and included the states setSearchedOrgs and setSearchInput

--- client/src/pages/SearchOrgs.js
+++ client/src/pages/SearchOrgs.js
@@ -46,9 +46,31 @@ const SearchOrgs = () => {
     };
 
    return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <p class="text-gray-700 text-base"></p>
-  </div>
+    <form onSubmit={handleFormSubmit} className="w-full max-w-sm">
+        <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                    Search Non-Profit
+                </label>
+            </div>
+            <div className="md:w-2/3">
+                <input
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    id="inline-full-name" type="text" value="Key word Search"></input>
+            </div>
+        </div>       
+        <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+                <button
+                    className="shadow bg-teal-700 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                    type="button">
+                    Search
+                </button>
+            </div>
+        </div>
+    </form>
    //nonprofits.id
    //nonprofits.name
    //nonprofits.description

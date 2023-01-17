122d357585c09abaade8b9c9850dce886139f339
ReTims <renatatims@gmail.com>
2023-01-17 12:52:39 -0600

    Added logic to Cart's local storage array - used useState + useMutation and useEffect to save non-profits to the Cart's array

--- client/src/pages/SearchOrgs.js
+++ client/src/pages/SearchOrgs.js
@@ -8,9 +8,18 @@ import Auth from "../utils/auth";
 // Apollo useMutation() Hook
 import { useMutation } from "@apollo/client";
 import { SAVE_NONPROFIT } from "../utils/mutations";
+
+//***User's favorites non-profits***/
 import {
   saveNonProfitsIds,
   getSavedNonProfitsIds,
+} from "../utils/localStorage";
+
+//***Cart***/
+import { ADD_NONPROFIT } from "../utils/mutations";
+import {
+  addNonProfitsIds,
+  getAddedNonProfitsIds,
 } from "../utils/localStorage";
 
 const SearchOrgs = () => {
@@ -19,18 +28,37 @@ const SearchOrgs = () => {
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState("");
 
+
+  //PROFILE//
   //saved Org values
   const [savedNonProfitIds, setSavedNonProfitIds] = useState(
     getSavedNonProfitsIds()
   );
 
-  //saveNonProfit mutation
+  //saveNonProfit mutation - add non-Profit to user's profile
   const [saveNonProfit, { error }] = useMutation(SAVE_NONPROFIT);
 
   // useEffect to save nonProfits Ids list to local Storage
   useEffect(() => {
     return () => saveNonProfitsIds(savedNonProfitIds);
   });
+
+
+  //CART//
+  
+  //added non-profit values - Cart
+  const [addedNonProfitIds, setAddedNonProfitIds] = useState(
+    getAddedNonProfitsIds()
+  );
+
+  //addNonProfit mutation - to add non-Profit to Cart
+  const [addNonProfit, {err}] = useMutation(ADD_NONPROFIT);
+  
+  // useEffect to save nonProfits Ids list to local Storage
+  useEffect(() => {
+    return () => addNonProfitsIds(addedNonProfitIds);
+  });
+
 
   // create method to search for nonProfits and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -71,7 +99,7 @@ const SearchOrgs = () => {
     }
   };
 
-  // create function to handle saving a book to our database
+  // PROFILE PAGE - create function to handle saving a non-profit to our database
   const handleSaveNonProfit = async (orgsId) => {
     const nonProfitToSave = searchedOrgs.find(
       (nonprofits) => nonprofits.orgsId === orgsId
@@ -95,6 +123,30 @@ const SearchOrgs = () => {
     }
   };
 
+  // CART - create function to handle adding a non-profit to our database -
+  const handleAddNonProfit = async (orgsId) => {
+    const nonProfitToAdd = searchedOrgs.find(
+      (nonprofits) => nonprofits.orgsId === orgsId
+    );
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    console.log(nonProfitToAdd);
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const { data } = await addNonProfit({
+        variables: { nonProfitData: { ...nonProfitToAdd } },
+      });
+      console.log(addedNonProfitIds);
+      // if nonProfit successfully saves to user's account, save nonProfit id to state
+      setAddedNonProfitIds([...addedNonProfitIds, nonProfitToAdd.orgsId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <>
       <div>
@@ -104,115 +156,115 @@ const SearchOrgs = () => {
           </label>
         </div>
 
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex">
-          <label
-            for="search-dropdown"
-            className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Your Email
-          </label>
-          <button
-            id="dropdown-button"
-            data-dropdown-toggle="dropdown"
-            className="z-10 inline-flex flex-shrink-0 items-center rounded-l-lg border border-gray-300 bg-gray-100 py-2.5 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-700"
-            type="button"
-          >
-            All categories{" "}
-            <svg
-              aria-hidden="true"
-              className="ml-1 h-4 w-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+        <form onSubmit={handleFormSubmit}>
+          <div className="flex">
+            <label
+              for="search-dropdown"
+              className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div
-            id="dropdown"
-            className="z-10 hidden w-44 divide-y divide-gray-100 rounded bg-white shadow dark:bg-gray-700"
-            data-popper-reference-hidden=""
-            data-popper-escaped=""
-            data-popper-placement="top"
-            //style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(897px, 5637px, 0px);"
-          >
-            <ul
-              className="py-1 text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="dropdown-button"
-            >
-              <li>
-                <button
-                  type="button"
-                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Food Banks
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Education
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Mental Health
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Environment
-                </button>
-              </li>
-            </ul>
-          </div>
-          <div className="relative w-full">
-            <input
-              onChange={(e) => setSearchInput(e.target.value)}
-              type="search"
-              id="search-dropdown"
-              className="z-20 block w-full rounded-r-lg border border-l-2 border-gray-300 border-l-gray-50 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:border-l-gray-700  dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
-              placeholder="Search Mockups, Logos, Design Templates..."
-              required
-            ></input>
+              Your Email
+            </label>
             <button
-              type="submit"
-              className="absolute top-0 right-0 rounded-r-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              id="dropdown-button"
+              data-dropdown-toggle="dropdown"
+              className="z-10 inline-flex flex-shrink-0 items-center rounded-l-lg border border-gray-300 bg-gray-100 py-2.5 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+              type="button"
             >
+              All categories{" "}
               <svg
                 aria-hidden="true"
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
+                className="ml-1 h-4 w-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
                 ></path>
               </svg>
-              <span className="sr-only">Search</span>
             </button>
+            <div
+              id="dropdown"
+              className="z-10 hidden w-44 divide-y divide-gray-100 rounded bg-white shadow dark:bg-gray-700"
+              data-popper-reference-hidden=""
+              data-popper-escaped=""
+              data-popper-placement="top"
+              //style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(897px, 5637px, 0px);"
+            >
+              <ul
+                className="py-1 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdown-button"
+              >
+                <li>
+                  <button
+                    type="button"
+                    className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Food Banks
+                  </button>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Education
+                  </button>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Mental Health
+                  </button>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Environment
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div className="relative w-full">
+              <input
+                onChange={(e) => setSearchInput(e.target.value)}
+                type="search"
+                id="search-dropdown"
+                className="z-20 block w-full rounded-r-lg border border-l-2 border-gray-300 border-l-gray-50 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:border-l-gray-700  dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
+                placeholder="Search Mockups, Logos, Design Templates..."
+                required
+              ></input>
+              <button
+                type="submit"
+                className="absolute top-0 right-0 rounded-r-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+                <span className="sr-only">Search</span>
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
-     </div> 
+        </form>
+      </div>
 
       <div className="flex justify-center">
         <h2>
@@ -280,7 +332,7 @@ const SearchOrgs = () => {
                         <button
                           type="button"
                           className=" inline-block rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-                          onClick={() => handleSaveNonProfit(nonprofits.orgsId)}
+                          onClick={() => handleAddNonProfit(nonprofits.orgsId)}
                         >
                           Donate List
                         </button>

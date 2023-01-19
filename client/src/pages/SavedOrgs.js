e14a6d1d5fe1d6cf853afb6ac0812340c5e32552
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2023-01-18 21:44:08 -0700

    Working to allow donate button functionality

--- client/src/pages/SavedOrgs.js
+++ client/src/pages/SavedOrgs.js
@@ -5,7 +5,7 @@ import { QUERY_USER } from "../utils/queries";
 
 // Remove Non Profit mutation
 import { REMOVE_NONPROFIT } from "../utils/mutations";
-
+import handleAddNonProfit from "./SearchOrgs";
 import Auth from "../utils/auth";
 import { removeNonProfitId } from "../utils/localStorage";
 
@@ -129,6 +129,7 @@ const SavedOrgs = () => {
                           <button
                             type="button"
                             className=" inline-block rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+                            onClick={() => handleAddNonProfit(nonprofits.orgsId)}
                           >
                             Donate List
                           </button>

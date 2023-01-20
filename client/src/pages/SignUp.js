47a8b20d508cd46bd8710cfe0494690cbdc74c51
Briggoh <H.e.briggs99@gmail.com>
2023-01-19 18:59:10 -0800

    added MOREE uirky buttons

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -160,7 +160,7 @@ const Signup = () => {
                 </div>
                 <button
                   type="submit"
-                  class="rounded-full bg-cyan-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+                  class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 >
                   Signup
                 </button>

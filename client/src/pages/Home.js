8b47d6bf888a802dc9f2fedb1ecc149325c805ee
ReTims <renatatims@gmail.com>
2023-03-23 18:24:20 -0500

    Included link to categories on homepage to the Search Page + styling with Tailwind - added animation to categories

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,48 +1,119 @@
 import React from "react";
 function Home() {
-    return (
-        <div className="bg-teal-50 dark:bg-gray-900">
-            <main className="mx-auto dark:bg-gray-900">
-                <section id="hero" className="bg-teal-50 flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12 scroll-mt-40 widescreen:section-min-height tallscreen:section-min-height bg-white dark:bg-gray-900">
-                    <article className="sm:w-1/2">
-                        <h2 className="max-w-md text-4xl font-bold text-center sm:text-5sl sm:text-left text-slate-900 dark:text-white hover:motion-safe:animate-bounce">
-                            When You Give, <span className=" text-indigo-700 dark:text-indigo-300">
-                            You Also Receive! </span>
-                        </h2>
-                        <p className="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400"> Ready to give back to the community in an unique way?</p>
-                        <p className="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400"></p>
-                    </article>
-                    <img src={require("../assets/img/hero.png")} className="w-1/2" alt="hero"></img>
-                </section>
-            <hr className="mx-auto bg-black dark:bg-white"></hr>
-                <section id="orgs" className="p-6 my-12">
-                    <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white"> Browse Our Organizations</h2>
-                    <ul className="list-none mx-auto my-12 flex flex-col sm:flex-row items-center gap-8">
-                        <li className="w-2/3 sm: w-5/6 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white py-6 px-2 rounded-3xl shadow-xl dark:bg-black">
-                            <img src={require("../assets/img/brain.png")} alt="yeh" className="w-1/2 mb-6"></img>
-                            <h3 className="hidden text-3xl text-center text-slate-500 dark:text-white"> Mental Health</h3>
-                            <p className="text-3xl text-center text-slate-900 mt-2 dark:text-slate-500"> Mental Health</p>
-                            <p className="sm:hidden text-2xl text-cente text-slate-900 mt-2 dark:text-slate-500"> View All</p>
-                        </li>
-                        <li className="w-2/3 sm: w-5/6 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white py-6 px-2 rounded-3xl shadow-xl dark:bg-black">
-                            <img src={require("../assets/img/earth.png")}alt="yeh" className="w-1/2 mb-6"></img>
-                            <h3 className="hidden text-3xl text-center text-slate-500 dark:text-white"> Mental Health</h3>
-                            <p className="text-3xl text-center text-slate-900 mt-2 dark:text-slate-500"> Conservation</p>
-                            <p className="sm:hidden text-2xl text-cente text-slate-900 mt-2 dark:text-slate-500"> View All</p>
-                        </li>
-                        <li className="w-2/3 sm: w-5/6 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white py-6 px-2 rounded-3xl shadow-xl dark:bg-black">
-                            <img src={require("../assets/img/puzzle.png")} alt="yeh" className="w-1/2 mb-6"></img>
-                            <h3 className="hidden text-3xl text-center text-slate-500 dark:text-white"> Mental Health</h3>
-                            <p className="text-3xl text-center text-slate-900 mt-2 dark:text-slate-500"> Education</p>
-                            <p className="sm:hidden text-2xl text-cente text-slate-900 mt-2 dark:text-slate-500"> View All</p>
-                        </li>
-                    </ul>
-                </section>
-            <hr className="mx-auto bg-black dark:bg-white"></hr>
-            </main>
-        </div>
-    );
+  return (
+    <div className="bg-teal-50 dark:bg-gray-900">
+      <main className="mx-auto dark:bg-gray-900">
+        <section
+          id="hero"
+          className="widescreen:section-min-height tallscreen:section-min-height mb-12 flex scroll-mt-40 flex-col-reverse items-center justify-center gap-8 bg-teal-50 bg-white p-6 dark:bg-gray-900 sm:flex-row"
+        >
+          <article className="sm:w-1/2">
+            <h2 className="sm:text-5sl text-slate-900 max-w-md text-center text-4xl font-bold hover:motion-safe:animate-bounce dark:text-white sm:text-left">
+              When You Give,{" "}
+              <span className=" text-indigo-700 dark:text-indigo-300">
+                You Also Receive!{" "}
+              </span>
+            </h2>
+            <p className="text-slate-700 dark:text-slate-400 mt-4 max-w-md text-center text-2xl sm:text-left">
+              {" "}
+              Ready to give back to the community in an unique way?
+            </p>
+            <p className="text-slate-700 dark:text-slate-400 mt-4 max-w-md text-center text-2xl sm:text-left"></p>
+          </article>
+          <img
+            src={require("../assets/img/hero.png")}
+            className="w-1/2"
+            alt="hero"
+          ></img>
+        </section>
+        <hr className="mx-auto bg-black dark:bg-white"></hr>
+        <section id="orgs" className="my-12 p-6">
+          <a href="/SearchOrgs">
+            <h2 className="text-slate-900 mb-6 text-center text-4xl font-bold hover:motion-safe:animate-bounce dark:text-white sm:text-5xl">
+              {" "}
+              Browse Organizations
+            </h2>
+          </a>
+          <ul className="mx-auto my-12 flex list-none flex-col items-center justify-center gap-8 sm:flex-row">
+            <li className="sm:flex sm:w-2/3">
+              <a
+                href="/SearchOrgs"
+                className="flex transform flex-col items-center rounded-3xl border border-solid bg-white py-6 px-2 shadow-lg transition duration-300 hover:-translate-y-4 hover:shadow-xl dark:border-gray-100 dark:bg-black"
+              >
+                <img
+                  src={require("../assets/img/brain.png")}
+                  alt="imageBrain"
+                  className="mb-6 w-1/2"
+                ></img>
+                <h3 className="text-slate-500 hidden text-center text-3xl dark:text-white">
+                  {" "}
+                  Mental Health
+                </h3>
+                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-3xl">
+                  {" "}
+                  Mental Health
+                </p>
+                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-2xl sm:hidden">
+                  {" "}
+                  View All
+                </p>
+              </a>
+            </li>
+            <li className="sm:flex sm:w-2/3">
+              <a
+                href="/SearchOrgs"
+                className="flex transform flex-col items-center rounded-3xl border border-solid bg-white py-6 px-2 shadow-lg transition duration-300 hover:-translate-y-4 hover:shadow-xl dark:border-gray-100 dark:bg-black"
+              >
+                <img
+                  src={require("../assets/img/earth.png")}
+                  alt="imageEarth"
+                  className="mb-6 w-1/2"
+                ></img>
+                <h3 className="text-slate-500 hidden text-center text-3xl dark:text-white">
+                  {" "}
+                  Mental Health
+                </h3>
+                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-3xl">
+                  {" "}
+                  Conservation
+                </p>
+                <p className="text-cente text-slate-900 dark:text-slate-500 mt-2 text-2xl sm:hidden">
+                  {" "}
+                  View All
+                </p>
+              </a>
+            </li>
+
+            <li className="sm:flex sm:w-2/3">
+              <a
+                href="/SearchOrgs"
+                className="flex transform flex-col items-center rounded-3xl border border-solid bg-white py-6 px-2 shadow-lg transition duration-300 hover:-translate-y-4 hover:shadow-xl dark:border-gray-100 dark:bg-black"
+              >
+                <img
+                  src={require("../assets/img/puzzle.png")}
+                  alt="imagePuzzle"
+                  className="mb-6 w-1/2"
+                ></img>
+                <h3 className="text-slate-500 hidden text-center text-3xl dark:text-white">
+                  {" "}
+                  Mental Health
+                </h3>
+                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-3xl">
+                  {" "}
+                  Education
+                </p>
+                <p className="text-cente text-slate-900 dark:text-slate-500 mt-2 text-2xl sm:hidden">
+                  {" "}
+                  View All
+                </p>
+              </a>
+            </li>
+          </ul>
+        </section>
+        <hr className="mx-auto bg-black dark:bg-white"></hr>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
-

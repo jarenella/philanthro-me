819e67b51b196a44bb6c04648bc3186de6d5a6d5
ReTims <renatatims@gmail.com>
2023-03-27 11:44:31 -0500

    Added a catch-all route handler in server + catch-all in App.js and fixed a Login link

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -166,12 +166,12 @@ const Signup = () => {
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Already have an account?{" "}
-                  <a
-                    href="/LogIn"
+                  <Link
+                    to="/LogIn"
                     className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
                   >
                     Login here
-                  </a>
+                  </Link>
                 </p>
               </form>
             )}
--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
+// catch-all route handler
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {

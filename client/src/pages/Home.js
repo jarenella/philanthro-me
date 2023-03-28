15fddb26025183cf9f43a99e839bfc1434584e51
ReTims <renatatims@gmail.com>
2023-03-28 13:30:16 -0500

    Home page adjustments. Co-authored-by: jarenella <jamesa122333@gmail.com>

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,13 +4,12 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="bg-teal-50 dark:bg-gray-900">
-      <main className="mx-auto dark:bg-gray-900">
+      <main className="mx-auto dark:bg-gray-900 min-h-screen container ">
         <section
           id="hero"
-          className="widescreen:section-min-height tallscreen:section-min-height mb-12 flex scroll-mt-40 flex-col-reverse items-center justify-center gap-8 bg-teal-50 bg-white p-6 dark:bg-gray-900 sm:flex-row"
-        >
-          <article className="sm:w-1/2">
-            <h2 className="sm:text-5sl text-slate-900 max-w-md text-center text-4xl font-bold hover:motion-safe:animate-bounce dark:text-white sm:text-left">
+          className="flex scroll-mt-50 flex-col-reverse items-center justify-center gap-1 bg-teal-50 bg-white px:0 dark:bg-gray-900 sm:flex-row ">
+          <article className="xs:w-1/3 sm:w-1/2">
+            <h2 className="sm:text-4xl text-slate-900 max-w-md text-center text-4xl font-bold hover:motion-safe:animate-bounce dark:text-white sm:text-left">
               When You Give,{" "}
               <span className=" text-indigo-700 dark:text-indigo-300">
                 You Also Receive!{" "}
@@ -24,20 +23,19 @@ function Home() {
           </article>
           <img
             src={require("../assets/img/hero.png")}
-            className="w-1/2"
+            className="xs: w-1/2 sm:w-1/4"
             alt="hero"
           ></img>
         </section>
-        <hr className="mx-auto bg-black dark:bg-white"></hr>
-        <section id="orgs" className="my-12 p-6">
+        <hr className="mx-auto bg-black dark:bg-white m-3"></hr>
+        <section id="orgs" className="p-4">
           <Link to="/SearchOrgs">
-            <h2 className="text-slate-900 mb-6 text-center text-4xl font-bold hover:motion-safe:animate-bounce dark:text-white sm:text-5xl">
-              {" "}
+            <h2 className="text-slate-900 mb-6 text-center text-4xl font-bold hover:motion-safe:animate-bounce dark:text-white ">
               Browse Organizations
             </h2>
           </Link>
-          <ul className="mx-auto my-12 flex list-none flex-col items-center justify-center gap-8 sm:flex-row">
-            <li className="sm:flex sm:w-2/3">
+          <ul className=" flex list-none items-center justify-center gap-8 sm:flex-row">
+            <li >
               <Link
                 to="/SearchOrgs"
                 className="flex transform flex-col items-center rounded-3xl border border-solid bg-white py-6 px-2 shadow-lg transition duration-300 hover:-translate-y-4 hover:shadow-xl dark:border-gray-100 dark:bg-black"
@@ -45,23 +43,14 @@ function Home() {
                 <img
                   src={require("../assets/img/brain.png")}
                   alt="imageBrain"
-                  className="mb-6 w-1/2"
+                  className="w-1/2 xs:w-3/4"
                 ></img>
-                <h3 className="text-slate-500 hidden text-center text-3xl dark:text-white">
-                  {" "}
+                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-lg lg:text-3xl dark:text-white hidden sm:block">
                   Mental Health
-                </h3>
-                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-3xl">
-                  {" "}
-                  Mental Health
-                </p>
-                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-2xl sm:hidden">
-                  {" "}
-                  View All
                 </p>
               </Link>
             </li>
-            <li className="sm:flex sm:w-2/3">
+            <li >
               <Link
                 to="/SearchOrgs"
                 className="flex transform flex-col items-center rounded-3xl border border-solid bg-white py-6 px-2 shadow-lg transition duration-300 hover:-translate-y-4 hover:shadow-xl dark:border-gray-100 dark:bg-black"
@@ -69,24 +58,15 @@ function Home() {
                 <img
                   src={require("../assets/img/earth.png")}
                   alt="imageEarth"
-                  className="mb-6 w-1/2"
+                  className="w-1/2  xs:w-3/4"
                 ></img>
-                <h3 className="text-slate-500 hidden text-center text-3xl dark:text-white">
-                  {" "}
-                  Mental Health
-                </h3>
-                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-3xl">
-                  {" "}
+                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-lg lg:text-3xl dark:text-white hidden sm:block">
                   Conservation
-                </p>
-                <p className="text-cente text-slate-900 dark:text-slate-500 mt-2 text-2xl sm:hidden">
-                  {" "}
-                  View All
                 </p>
               </Link>
             </li>
 
-            <li className="sm:flex sm:w-2/3">
+            <li >
               <Link
                 to="/SearchOrgs"
                 className="flex transform flex-col items-center rounded-3xl border border-solid bg-white py-6 px-2 shadow-lg transition duration-300 hover:-translate-y-4 hover:shadow-xl dark:border-gray-100 dark:bg-black"
@@ -94,25 +74,16 @@ function Home() {
                 <img
                   src={require("../assets/img/puzzle.png")}
                   alt="imagePuzzle"
-                  className="mb-6 w-1/2"
+                  className="w-1/2  xs:w-3/4"
                 ></img>
-                <h3 className="text-slate-500 hidden text-center text-3xl dark:text-white">
-                  {" "}
-                  Mental Health
-                </h3>
-                <p className="text-slate-900 dark:text-slate-500 mt-2 text-center text-3xl">
-                  {" "}
+                <p className="text-slate-700 dark:text-slate-500 mt-2 text-center text-lg lg:text-3xl dark:text-white hidden sm:block">
                   Education
-                </p>
-                <p className="text-cente text-slate-900 dark:text-slate-500 mt-2 text-2xl sm:hidden">
-                  {" "}
-                  View All
                 </p>
               </Link>
             </li>
           </ul>
         </section>
-        <hr className="mx-auto bg-black dark:bg-white"></hr>
+        <hr className="mx-auto bg-black dark:bg-white m-6"></hr>
       </main>
     </div>
   );
